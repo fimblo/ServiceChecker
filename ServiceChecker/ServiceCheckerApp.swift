48e64aa258b0083a80dffba38d490bace515b756
Mattias Jansson <fimblo@yanson.org>
2025-02-10 16:45:26 +0100

    Initial Commit

---
+++ ServiceChecker/ServiceCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceCheckerApp.swift
+//  ServiceChecker
+//
+//  Created by fimblo on 2025-02-10.
+//
+
+import SwiftUI
+
+@main
+struct ServiceCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
