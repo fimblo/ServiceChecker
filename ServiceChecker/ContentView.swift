4c1a2d6a4b23cd595a228bf8a7725cdabf89de94
Mattias Jansson <fimblo@yanson.org>
2025-02-12 19:10:27 +0100

    refactor: move datamodels to own file

--- ServiceChecker/ContentView.swift
+++ ServiceChecker/ContentView.swift
@@ -8,21 +8,6 @@ import SwiftUI
 import AppKit
 import Foundation
 
-/// Represents the status of a single service being monitored
-struct ServiceStatus: Identifiable {
-    let id = UUID()
-    let name: String
-    let url: String
-    var status: Bool
-    var lastError: String = ""  // Added to store error messages
-}
-
-/// Represents a service configuration that can be saved to disk
-struct ServiceConfig: Codable {
-    let name: String
-    let url: String
-}
-
 /// Controls the status bar menu and service monitoring
 class StatusBarController: NSObject, ObservableObject {
     @Published var services: [ServiceStatus] = ServiceUtils.loadServicesFromFile()
---
+++ ServiceChecker/Models/ServiceModels.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+/// Represents the status of a single service being monitored
+struct ServiceStatus: Identifiable {
+    let id = UUID()
+    let name: String
+    let url: String
+    var status: Bool
+    var lastError: String = ""  // Added to store error messages
+}
+
+/// Represents a service configuration that can be saved to disk
+struct ServiceConfig: Codable {
+    let name: String
+    let url: String
+} 
