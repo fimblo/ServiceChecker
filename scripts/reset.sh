6249965424f435c1d45807bb490f2cec0534fee3
Mattias Jansson <fimblo@yanson.org>
2025-02-13 21:20:02 +0100

    chore: add script to reset prefs

---
+++ scripts/reset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Resetting ServiceChecker preferences"
+
+rm -rf /Users/fimblo/Library/Containers/org.yanson.ServiceChecker/Data/Library/Application Support/ServiceChecker
+rm -rf ~/Library/Saved\ Application\ State/org.yanson.ServiceChecker.savedState
+rm -rf ~/Library/Caches/org.yanson.ServiceChecker
+
+defaults delete org.yanson.ServiceChecker
