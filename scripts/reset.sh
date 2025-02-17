eff87ba991420bedea6fc1cad131005798b3b4e3
Mattias Jansson <fimblo@yanson.org>
2025-02-17 11:20:52 +0100

    fix: generalise script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -2,7 +2,7 @@
 
 echo "Resetting ServiceChecker preferences"
 
-rm -rf /Users/fimblo/Library/Containers/org.yanson.ServiceChecker/Data/Library/Application Support/ServiceChecker
+rm -rf ~/Library/Containers/org.yanson.ServiceChecker/Data/Library/Application Support/ServiceChecker
 rm -rf ~/Library/Saved\ Application\ State/org.yanson.ServiceChecker.savedState
 rm -rf ~/Library/Caches/org.yanson.ServiceChecker
 
